56c80408e4b45038f09aa2fc7b2f1511a0126f13
shaloo <5890484+shaloo@users.noreply.github.com>
2023-08-28 12:12:24 -0400

    Update to the latest auth sdk v1.0.8-beta.3

--- src/auth.js
+++ src/auth.js
@@ -2,12 +2,7 @@ import { AuthProvider } from "@arcana/auth";
 
 let authInstance;
 
-//Dev ClientId
-//const clientId = "9527cdf585cd31d0bd06bfc1b008accea781404";
-
-//Mainnet ClientId
-// App: 'Arc4n4-docx'  ; wallet: '0xbd1127C076c91274B9Ccd6c506817D364bc7ff80'
-const clientId = "xar_live_d7c88d9b033d100e4200d21a5c4897b896e60063";
+const clientId = "xar_dev_19527cdf585cd31d0bd06bfc1b008accea781404";
 
 export async function getAuthInstance() {
   if (authInstance == null) {
@@ -15,6 +10,9 @@ export async function getAuthInstance() {
       alwaysVisible: true,
       theme: "light",
       position: "right",
+      connectOptions: {
+        compact: true
+      }
     });
     await authInstance.init();
     await authInstance.connect();
